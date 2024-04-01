ed2d8932605ad6ce6185f32ec4df96abfb07cad5
Akshita Tiwari <85664297+tiwari1@users.noreply.github.com>
2024-04-01 15:56:06 +0530

    Add files via upload

---
+++ Leap year or Not.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=int(input("enter the year"))
+if x%4==0:
+    print("The year is a leap year")
+else:
+    print("Not a leap year")
+
+
+# In[ ]:
+
+
+
+
